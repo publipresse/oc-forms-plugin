da5159915273112923a4114892885dfddb6375dd
PubliAlex <alexandre@publipresse.fr>
2023-05-30 21:41:04 +0200

    Fix issue in recaptcha.js

--- assets/js/recaptcha.js
+++ assets/js/recaptcha.js
@@ -30,8 +30,6 @@ function resetReCaptcha(form) {
 function submitReCaptcha(form) {
     const submit = form.querySelector('[type="submit"]');
     const el = form.querySelector('.g-recaptcha');
-    console.log(submit);
-    console.log(captcha);
     submit.addEventListener('click', function(e) {
         e.preventDefault();
         grecaptcha.execute(el);
